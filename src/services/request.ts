21ca6fc7edc3ff31852edf60663543df48b3ffff
soheyHashimoto <heyso1209@gmail.com>
2022-06-25 03:22:21 +0900

    [MODIFY]url修正

--- src/services/request.ts
+++ src/services/request.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from "axios";
 export function requestDiscordWebhook(): void {
   const options: AxiosRequestConfig = {
     method: "POST",
-    url: "https://discord.com/api/webhooks/989950740145516595/PDgWyUkErpiWXzJ7DHG8y8i4nyYzt9Xl_gj5fMEcNcQQ-lrV0WmM3x_qHtZyLcF4l-qk",
+    url: "https://discord.com/api/webhooks/989958429844066314/_aSThE6SAZjXWmOm93ttMlL8Z0xX4UOWMiNXG2tWiqaGCoFs25Qgy4Wkl7y2E3OP7SII",
     data: {
       content: "Hello World!",
     },
