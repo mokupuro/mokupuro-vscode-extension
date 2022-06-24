aca78a97d91d6974e56f0a306f13e8a814970f29
soheyHashimoto <heyso1209@gmail.com>
2022-06-25 01:22:21 +0900

    [ADD]サイドバーに画像が表示されるようにする

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { SideBarView } from "./views/sidebar";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,10 +15,15 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from mokupuro-vscode-extension!');
+
+    const sbv = new SideBarView();
+    vscode.window.registerTreeDataProvider("mokupuro-vscode", sbv);
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
