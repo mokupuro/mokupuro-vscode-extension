56b6a9e29e9ec1c37c2243b214357d5c2a608510
soheyHashimoto <heyso1209@gmail.com>
2022-06-25 03:24:57 +0900

    [MODIFY]送信する文言修正

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,11 +1,12 @@
 import axios, { AxiosRequestConfig } from "axios";
+import { DISCORD_WEBHOOK_URL } from "../constants/env";
 
 export function requestDiscordWebhook(): void {
   const options: AxiosRequestConfig = {
     method: "POST",
     url: DISCORD_WEBHOOK_URL,
     data: {
-      content: "Hello World!",
+      content: "メンバーが稼働開始しました",
     },
     headers: {
       "Content-Type": "application/json",
