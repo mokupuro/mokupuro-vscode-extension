bcbf8f89b440ac9df69740d8b1930d234f9e4ac0
soheyHashimoto <heyso1209@gmail.com>
2022-06-25 02:59:08 +0900

    [ADD]稼働を報告する

--- src/views/sidebar.ts
+++ src/views/sidebar.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from "axios";
 
 // TODO: anyの排除
 export class SideBarView {
@@ -10,11 +11,29 @@ export class SideBarView {
     if (element) {
       return [];
     }
+    const item = new vscode.TreeItem("稼働を報告");
+    item.command = {
+      command: "notify.discord",
+      title: "稼働報告",
+    };
 
-    return [
-      new vscode.TreeItem("1"),
-      new vscode.TreeItem("2"),
-      new vscode.TreeItem("3"),
-    ];
+    return [item];
+  }
+
+  notifyDiscord() {
+    const options: AxiosRequestConfig = {
+      method: "POST",
+      url: "https://discord.com/api/webhooks/989950740145516595/PDgWyUkErpiWXzJ7DHG8y8i4nyYzt9Xl_gj5fMEcNcQQ-lrV0WmM3x_qHtZyLcF4l-qk",
+      data: {
+        content: "Hello World!",
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    axios(options);
+
+    vscode.window.showInformationMessage("稼働報告しました");
   }
 }
